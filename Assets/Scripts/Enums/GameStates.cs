a15df1bbaf00344d599b00bd5bafe2463003b923
Jesus Guerra <jesus.guerra@alegra.com>
2025-01-25 08:57:36 -0400

    feat: init implement player pref controller

---
+++ Assets/Scripts/Enums/GameStates.cs
@@ -0,0 +1,9 @@
+public enum GameState {
+    MainMenu,
+    FirstLevel,
+    SecondLevel,
+    ThirdLevel,
+    GameOver
+
+}
+
---
+++ Assets/Scripts/PlayerPrefController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPrefController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private GameState gt;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
