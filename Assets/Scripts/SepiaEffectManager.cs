75f0cd02cf14fa72d43f9edfea4d4f3f26851019
Gabriel Guerra <gfranklings@gmail.com>
2025-01-24 23:21:02 -0400

    feat: `SepiaEffectManager` now have helpers for dealing with the alpha channel

--- Assets/Scripts/SepiaEffectManager.cs
+++ Assets/Scripts/SepiaEffectManager.cs
@@ -1,21 +1,43 @@
+using System;
 using UnityEngine;
 
 public class SepiaEffectManager : MonoBehaviour
 {
-    public GameObject sepiaElement;
+    private SpriteRenderer _spriteRenderer;
+
+    private void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+
+        if (_spriteRenderer == null)
+        {
+            Debug.Log("Unable to find component sprite renderer for sepia effect manager");
+            return;
+        }
+    }
 
     public void Enable()
     {
-        sepiaElement.SetActive(true);
+        gameObject.SetActive(true);
     }
 
     public void Disable()
     {
-        sepiaElement.SetActive(false);
+        gameObject.SetActive(false);
     }
 
     public void ToggleVisibility()
     {
-        sepiaElement.SetActive(!sepiaElement.activeSelf);
+        gameObject.SetActive(!gameObject.activeSelf);
+    }
+
+    public void SetAlphaChannel(float newAlpha)
+    {
+        _spriteRenderer.color = new Color(
+            _spriteRenderer.color.r,
+            _spriteRenderer.color.g,
+            _spriteRenderer.color.b,
+            newAlpha
+        );
     }
 }
