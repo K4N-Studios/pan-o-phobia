9362d9e57c4e142067679c4b32616a45ad04f849
Gabriel Guerra <gfranklings@gmail.com>
2025-01-25 20:28:29 -0400

    feat: Enemy path finding will now tryna go take the player if they are around the enemy

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -3,16 +3,34 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    public enum MovementType { Stationary, Patrol, Chase }
+    public enum EMovementType { Stationary, Patrol, Chase }
     public bool isWaiting = false;
+    public float stopChasingTimerDefaultTime = 3f;
 
-    [SerializeField] private MovementType _movementType = MovementType.Stationary;
+    private int _currentPatrolIndex = 0;
+    [SerializeField] private float _stopChasingTimerTime = 3f;
+    [SerializeField] private bool _stopChasingTimerRunning = false;
+
+    [SerializeField] private EMovementType _movementType = EMovementType.Stationary;
     [SerializeField] private Transform[] _patrolPoints;
     [SerializeField] private float _patrolSpeed = 2f;
     [SerializeField] private float _chaseSpeed = 4f;
     [SerializeField] private Transform _player;
     [SerializeField] private bool _isChasing = false;
-    private int _currentPatrolIndex = 0;
+    [SerializeField] private bool _trynaFollowPlayer = false;
+
+    public EMovementType MovementType => _movementType;
+
+    private void ResetStopChasingTimer()
+    {
+        _stopChasingTimerTime = stopChasingTimerDefaultTime;
+    }
+
+    private void CancelStopChasingTimer()
+    {
+        ResetStopChasingTimer();
+        _stopChasingTimerRunning = false;
+    }
 
     private void Start()
     {
@@ -23,7 +41,7 @@ public class EnemyMovement : MonoBehaviour
     {
         while (true)
         {
-            if (_movementType == MovementType.Patrol)
+            if (_movementType == EMovementType.Patrol)
             {
                 if (_isChasing)
                 {
@@ -38,9 +56,67 @@ public class EnemyMovement : MonoBehaviour
         }
     }
 
+    private void SwitchToChaseMode()
+    {
+        _isChasing = true;
+        _movementType = EMovementType.Chase;
+    }
+
+    private void SwitchToPatrolMode()
+    {
+        _isChasing = false;
+        _movementType = EMovementType.Patrol;
+    }
+
+    // This will make the timer restart when the trigger goes out.
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (_trynaFollowPlayer && other.gameObject.CompareTag("Player"))
+        {
+            if (_stopChasingTimerRunning)
+            {
+                Debug.Log("Stopping running timer");
+                CancelStopChasingTimer();
+            }
+
+            if (!_isChasing && _movementType == EMovementType.Patrol)
+            {
+                Debug.Log("Starting to follow player");
+                SwitchToChaseMode();
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (_trynaFollowPlayer && other.gameObject.CompareTag("Player"))
+        {
+            if (!_stopChasingTimerRunning)
+            {
+                _stopChasingTimerRunning = true;
+                _stopChasingTimerTime = stopChasingTimerDefaultTime;
+                Debug.Log("Collision timer has been reset and started");
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (_stopChasingTimerRunning == true)
+        {
+            _stopChasingTimerTime -= Time.deltaTime;
+            if (_trynaFollowPlayer && _stopChasingTimerTime <= 0.0f)
+            {
+                Debug.Log("Lost player after three seconds without no more collider impact");
+                CancelStopChasingTimer();
+                SwitchToPatrolMode();
+            }
+        }
+    }
+
     private IEnumerator Patrol()
     {
-        if (_patrolPoints.Length == 0)
+        if (_patrolPoints.Length == 0 || _isChasing)
         {
             yield break;
         }
@@ -49,6 +125,11 @@ public class EnemyMovement : MonoBehaviour
 
         while (Vector2.Distance(transform.position, point.position) > 0.1f)
         {
+            if (_isChasing)
+            {
+                yield break;
+            }
+
             MoveTowards(point.position, _patrolSpeed);
             yield return null;
         }
@@ -61,17 +142,16 @@ public class EnemyMovement : MonoBehaviour
         _currentPatrolIndex = (_currentPatrolIndex + 1) % _patrolPoints.Length;
     }
 
-
     public void HandleMovement()
     {
         switch (_movementType)
         {
-            case MovementType.Stationary:
+            case EMovementType.Stationary:
                 break;
-            case MovementType.Patrol:
+            case EMovementType.Patrol:
                 Patrol();
                 break;
-            case MovementType.Chase:
+            case EMovementType.Chase:
                 ChasePlayer();
                 break;
         }
