14ead187a6a9a199ddbb269249f4976702f81ba9
Gabriel Guerra <gfranklings@gmail.com>
2025-01-26 14:05:20 -0400

    feat: Detect player around interactable

--- Assets/Scripts/Collectables/Interactable.cs
+++ Assets/Scripts/Collectables/Interactable.cs
@@ -2,10 +2,24 @@ using UnityEngine;
 
 public class Interactable : MonoBehaviour
 {
-    [SerializeField] private LayerMask _playerLayer;
+    private void DrawInteractionMark()
+    {
+        var playerMask = LayerMask.GetMask("Player");
+        var playerCollision = Physics2D.OverlapCircle(transform.position, 1f, playerMask);
+
+        if (playerCollision != null)
+        {
+            Debug.Log("Player is being able to interact with element: " + gameObject.name);
+        }
+    }
+
+    private void Update()
+    {
+        DrawInteractionMark();
+    }
+
     public GameObject Interact()
     {
-        Debug.Log("Has interacted with " + gameObject.name + "!");
         return gameObject;
-    }    
+    }
 }
