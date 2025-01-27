1c60e9bb7ccc7f2678c7943f0d30249311cbfd64
Gabriel Guerra <gfranklings@gmail.com>
2025-01-27 17:40:39 -0400

    feat: Add sound effects for doors

--- Assets/Scripts/Scenary/DoorTeleporterController.cs
+++ Assets/Scripts/Scenary/DoorTeleporterController.cs
@@ -9,16 +9,34 @@ public class DoorTeleporterController : MonoBehaviour
 
     private FadeController _fadeController;
 
+    private FMOD.Studio.EventInstance _sfxCloseDoor;
+
+    [SerializeField] private float _fadeSeconds = 0.5f;
+
     private void Awake()
     {
         _fadeController = FindObjectOfType<FadeController>();
+        _sfxCloseDoor = FMODUnity.RuntimeManager.CreateInstance("event:/Actions/Door/Close");
+    }
+
+    private void TeleportTo(Vector3 position)
+    {
+        player.transform.position = position;
+        
+        if (_sfxCloseDoor.getPlaybackState(out FMOD.Studio.PLAYBACK_STATE playbackState) == FMOD.RESULT.OK)
+        {
+            if (playbackState == FMOD.Studio.PLAYBACK_STATE.STOPPED)
+            {
+                _sfxCloseDoor.start();
+            }
+        }
     }
 
     private IEnumerator OpenSequence()
     {
         _fadeController.TriggerFade();
-        yield return new WaitForSeconds(0.5f);
-        player.transform.position = secondDoor.transform.position;
+        yield return new WaitForSeconds(_fadeSeconds);
+        TeleportTo(secondDoor.transform.position);
     }
 
     public void HandleOnOpen()
@@ -29,13 +47,18 @@ public class DoorTeleporterController : MonoBehaviour
     private IEnumerator OpenSequenceEnd()
     {
         _fadeController.TriggerFade();
-        yield return new WaitForSeconds(0.5f);
-        player.transform.position = firstDoor.transform.position;
+        yield return new WaitForSeconds(_fadeSeconds);
+        TeleportTo(firstDoor.transform.position);
     }
 
     // for second door
     public void HandleOnOpenEnd()
     {
         StartCoroutine(OpenSequenceEnd());
+    }
+
+    private void Destroy()
+    {
+        _sfxCloseDoor.release();
     }
 }
