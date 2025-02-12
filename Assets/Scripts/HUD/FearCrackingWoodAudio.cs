9727a3959591b6049b078062b982d25eb61330de
Gabriel Guerra <gfranklings@gmail.com>
2025-02-11 20:36:01 -0400

    chore: Remove not needed logs

--- Assets/Scripts/HUD/FearCrackingWoodAudio.cs
+++ Assets/Scripts/HUD/FearCrackingWoodAudio.cs
@@ -13,8 +13,6 @@ public class FearCrackingWoodAudio : WithSongManager
         LocalLightsRegister localLightsTurnedOn = gameState.lightsStates.FindAll(x => x.IsOn == true).LastOrDefault();
         bool shouldStop = (localLightsTurnedOn != null && localLightsTurnedOn.IsOn == true) || gameState.duringGameOverSplash;
 
-        Debug.Log("should stop -> " + (shouldStop ? "yes" : "no"));
-
         // check for the existence of some local element turned on, using the last turned
         // on as it will be probably the only one that's turned on currently and where the user is at.
         // FIXME: As this could indirectly create a bug where the current light is at the mid position instead
